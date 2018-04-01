adba65952b9124fd9084579b81659cef4be527f7
Erik Higginbotham <higgi615@ufl.edu>
2018-04-01 19:30:35 -0400

    pls dont delete my work zoe

--- modules/studySpaces/client/controllers/list-studySpaces.client.controller.js
+++ modules/studySpaces/client/controllers/list-studySpaces.client.controller.js
@@ -26,5 +26,35 @@
         return '/modules/studySpaces/client/img/crowd3.png';
       }
     };
+
+    $scope.getNoiseIcon = function (noiseLevel) {
+      if (noiseLevel < 1.5) {
+        return '/modules/studySpaces/client/img/sound0.png';
+      }
+      else if (noiseLevel < 2.5) {
+        return '/modules/studySpaces/client/img/sound1.png';
+      }
+      else if (noiseLevel < 3.5) {
+        return '/modules/studySpaces/client/img/sound2.png';
+      }
+      else {
+        return '/modules/studySpaces/client/img/sound3.png';
+      }
+    };
+
+    $scope.getAtmoIcon = function (atmosphereLevel) {
+      if (atmosphereLevel < 1.5) {
+        return '/modules/studySpaces/client/img/atmo0.png';
+      }
+      else if (atmosphereLevel < 2.5) {
+        return '/modules/studySpaces/client/img/atmo1.png';
+      }
+      else if (atmosphereLevel < 3.5) {
+        return '/modules/studySpaces/client/img/atmo2.png';
+      }
+      else {
+        return '/modules/studySpaces/client/img/atmo3.png';
+      }
+    };
   }
 }());
